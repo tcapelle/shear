00f672e3558f5bfc18869522542b048114cc9de0
Thomas Capelle <tcapelle@pm.me>
2024-01-31 23:12:13 +0000

    fixed create model script

--- create_small_model.py
+++ create_small_model.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 import simple_parsing
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig
 
 @dataclass
 class Config(simple_parsing.Serializable):
@@ -8,11 +8,22 @@ class Config(simple_parsing.Serializable):
     output_name: str = "models/mistral_7b_12_layers_start"
     n_layers: int = 12
     save_tokenizer: bool = True
+    device_map: str = "cuda:0"
 
 config: Config = simple_parsing.parse(Config)
 
-model = AutoModelForCausalLM.from_pretrained(config.model_id)
-model.model.layers = model.model.layers[:config.n_layers]
+model_config = AutoConfig.from_pretrained(config.model_id)
+model_config.num_hidden_layers = config.n_layers
+print(model_config)
+
+model = AutoModelForCausalLM.from_pretrained(
+    config.model_id, 
+    config=model_config, 
+    torch_dtype='auto',
+    device_map=config.device_map)
+
+print(f"Total model Parameters: {(model.num_parameters()/1e6):.2f}M")
+
 model.save_pretrained(config.output_name)
 print(f"Saved model to {config.output_name}")
 if config.save_tokenizer:
