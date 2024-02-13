7e2dccfc0f8bef3423f28fdefcdcad6e68841e5a
Thomas Capelle <tcapelle@pm.me>
2024-02-13 20:09:54 +0000

    missing util

--- utils.py
+++ utils.py
@@ -17,3 +17,19 @@ def freeze(model, n_freeze, freeze_embed, module_name="layers"):
     if freeze_embed:
         embed_tokens = _find_mod(model, "embed_tokens")
         embed_tokens.weight.requires_grad_(False);
+
+
+def map_state_dict(original_model, layer_ids=[0,1,30,31], layer_naming="layers"):
+    "We will map the parameters of the original model layer_ids to the new model layer_ids"
+    name_mapping = {}
+    layer_mapping = {layer_id: i for i, layer_id in enumerate(layer_ids)}
+    print(f"Layer mapping: {layer_mapping}")
+    for name, _ in original_model.named_parameters():
+        if layer_naming in name:
+            layer_id = int(name.split(".")[2])
+            if layer_id in layer_ids:
+                new_name = name.replace(f"{layer_naming}.{layer_id}", f"{layer_naming}.{layer_mapping[layer_id]}")
+                name_mapping[name] = new_name
+        else:
+            name_mapping[name] = name
+    return name_mapping
